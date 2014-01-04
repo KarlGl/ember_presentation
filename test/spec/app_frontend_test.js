c303750da6dc6fc407dd316a712ecd1fa962f0dc
Karl Glaser <kg@papercloud.com.au>
2014-01-05 02:32:19 +1100

    test for active classes

--- test/spec/app_frontend_test.js
+++ test/spec/app_frontend_test.js
@@ -30,7 +30,22 @@ define(['../../lib/app/ember_frontend.js'], function(app) {
     app.loadMovementsEmber([]);
   };
 
-  test('loads a slide index and content by directly visiting the url with a slide index. Has correct slide count. Will show all the slides at the bottom.', function() {
+  test('clicking a slide makes it the main slide, and changes the active classes.', function() {
+    setupFrontend();
+    visit('/0');
+    expect(5);
+    hasContent(find("#slidecontent"), 'test slide 1');
+    ok(find(".slide-item-cont").first().hasClass('active'), '1st slide will be active.');
+    ok(!find(".slide-item-cont").last().hasClass('active'), '2nd slide will be inactive.');
+
+    click('.slide-item-cont:last');
+    wait().then(function() {
+      hasContent(find("#slidecontent"), 'test slide 2');
+      ok(find(".slide-item-cont").last().hasClass('active'), '2nd slide will be active.');
+    })
+  });
+
+  test('loads a slide index and content by directly visiting the url with a slide index. Has correct slide count. Will show all the slides at the bottom. Class will be active.', function() {
     setupFrontend();
     visit('/1');
     hasContent(find("#slideid"), 1);
@@ -39,6 +54,7 @@ define(['../../lib/app/ember_frontend.js'], function(app) {
     deepEqual(app.state.movements, [1])
     equal(find(".slide-item").length, 2, 'has all the slides in the app at the bottom.');
     hasContent(find(".slide-item").first(), 'test slide 1');
+    ok(find(".slide-item-cont").last().hasClass('active'), 'Class will be active.');
   });
 
 
